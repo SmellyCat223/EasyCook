0f8fa7295d924889180fb22c11130b342134d2db
SmellyCat223 <trangialinh@gmail.com>
2024-05-30 13:48:47 +0700

    Implement Register screen

---
+++ frontend/app/index.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import Index from './(tabs)/index';
+
+const App = () => {
+  return (
+    <View>
+      <Index />
+    </View>    
+  );
+};
+
+export default App;
