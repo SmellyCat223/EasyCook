1f566aab721df5f4f650bb725b4be8d5bb9b6693
ThamudayaWinBerry <157303753+ThamudayaWinBerry@users.noreply.github.com>
2024-06-02 17:43:05 +0700

    Revert "Registration and Signing In cleared"
    This reverts commit 05d52071ed9aa4db66a6c0ecc0c8f9b1ebf82021.

--- frontend/app/sign-up.tsx
+++ frontend/app/sign-up.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { Formik, FormikHelpers } from 'formik';
 import * as Yup from 'yup';
@@ -15,6 +15,8 @@ interface SignUpProps {
 }
 
 const SignUp: FC<SignUpProps> = ({ switchComponent }) => {
+
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const validationSchema = Yup.object().shape({
     email: Yup.string().required('Email is required'),
@@ -35,6 +37,7 @@ const SignUp: FC<SignUpProps> = ({ switchComponent }) => {
     { setSubmitting }: FormikHelpers<FormValues>
   ) => {
     try {
+      setErrorMessage(null);
       // Make a POST request to your backend endpoint
       const response = await axios.post('http://192.168.1.113:3000/api/user/register', values); // use backend port
 
