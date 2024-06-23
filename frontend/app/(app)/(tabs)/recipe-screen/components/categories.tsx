4f0dd07733a0c0ac7f89eac7fb3b27ad9420877a
ThamudayaWinBerry <157303753+ThamudayaWinBerry@users.noreply.github.com>
2024-06-23 23:39:49 +0700

    Updated categories style

--- frontend/app/(app)/(tabs)/recipe-screen/components/categories.tsx
+++ frontend/app/(app)/(tabs)/recipe-screen/components/categories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, ScrollView, Image, StyleSheet } from 'react-native';
-import { TouchableOpacity } from 'react-native'; // Use GestureHandler's TouchableOpacity for better touch handling
+import { TouchableOpacity } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const categoryData = [
     { name: 'Full Meal', image: 'https://images.app.goo.gl/w3CuJfpz4Whizz98A' },
@@ -23,7 +24,7 @@ export default function Categories() {
                             <View style={styles.imageContainer}>
                                 <Image
                                     source={{ uri: cat.image }}
-                                    style={styles.image}
+                                    style={[styles.image, { width: hp(6), height: hp(6) }]} // Set the height to 10% of the screen height
                                 />
                             </View>
                             <Text style={styles.text}>{cat.name}</Text>
@@ -54,13 +55,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#ccc',
     },
     image: {
-        width: 50, // Adjust the width and height as needed
-        height: 50,
+        width: 50, // Adjust the width as needed
         borderRadius: 999,
     },
     text: {
         fontSize: 12, // Adjust the font size as needed
-        color: '#333',
+        color: 'white',
         marginTop: 4,
     },
 });
