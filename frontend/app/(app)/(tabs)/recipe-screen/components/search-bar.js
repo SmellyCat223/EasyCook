e96e87b6b55fcff4ae67599d62a8dc53d6c2c20a
ThamudayaWinBerry <157303753+ThamudayaWinBerry@users.noreply.github.com>
2024-06-29 11:20:44 +0700

    Updated Styling of Search-Bar

--- frontend/app/(app)/(tabs)/recipe-screen/components/search-bar.js
+++ frontend/app/(app)/(tabs)/recipe-screen/components/search-bar.js
@@ -1,23 +1,3 @@
-/* import React from 'react';
-import { Text, View } from "react-native";
-import { Icon } from 'react-native-elements';
-import { styled } from 'nativewind';
-
-const Search = () => {
-    return (
-        <View className="p-1">
-            <View className="flex-row items-center bg-white rounded-full p-2">
-                <Icon name="search1" type="antdesign" color="#6b7280" size={16} />
-                <Text className="ml-2 flex-1 text-sm text-gray-600">Search Recipe</Text>
-                <Icon name="filter" type="antdesign" color="#6b7280" size={16} className="ml-2" />
-            </View>
-        </View>
-    );
-};
-
-export default Search;
-*/
-
 import React from 'react';
 import { TextInput, View } from "react-native";
 import { Icon } from 'react-native-elements';
@@ -30,7 +10,7 @@ const Search = ({ onSearch }) => {
     };
 
     return (
-        <View className="p-1">
+        <View className="p-3">
             <View className="flex-row items-center bg-white rounded-full p-2">
                 <Icon name="search1" type="antdesign" color="#6b7280" size={16} />
                 <TextInput
