441c16477c6eeea82554c071a38b30ad94558add
ThamudayaWinBerry <157303753+ThamudayaWinBerry@users.noreply.github.com>
2024-06-30 21:10:54 +0700

    refix

--- frontend/app/(app)/(tabs)/settings-screen/components/inventory.tsx
+++ frontend/app/(app)/(tabs)/settings-screen/components/inventory.tsx
@@ -11,6 +11,7 @@ const Inventory: React.FC = () => {
     const [items, setItems] = useState<Item[]>([]);
     const [searchQuery, setSearchQuery] = useState('');
     const [userId, setUserId] = useState<string | null>(null);
+    const router = useRouter();
 
     useEffect(() => {
         const fetchUserId = async () => {
@@ -65,9 +66,9 @@ const Inventory: React.FC = () => {
 
     return (
         <View className="flex-1 bg-stone-950">
-            <View className="px-4 py-2">
-                <Filter setSearchQuery={setSearchQuery} />
-            </View>
+                <View className="px-4 py-2">
+                    <Filter setSearchQuery={setSearchQuery} />
+                </View>
             <Body items={filteredItems} />
         </View>
     );
