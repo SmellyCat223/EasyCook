c14a139b7e08f31a094127eeb75111ac4fc8f893
SmellyCat223 <trangialinh@gmail.com>
2024-07-28 19:59:53 +0700

    Created autogrnerate grocery file - in progress

---
+++ frontend/app/(app)/(tabs)/mealPlanner-screen/components/autogenerate-grocery.tsx
@@ -0,0 +1,29 @@
+// import React, { useState, useEffect, useCallback } from 'react';
+// import { View, Alert, Modal, TouchableWithoutFeedback, Keyboard, Button } from "react-native";
+// import { supabase } from '../../../../supabase';
+// import { Item } from '../../../../types';
+// import { useFocusEffect } from '@react-navigation/native';
+
+// const AutogenerateGrocery: React.FC = () => {
+
+//     const [userId, setUserId] = useState<string | null>(null);
+//     const [shoppingListId, setShoppingListId] = useState<string | null>(null);
+//     const [inventoryId, setInventoryId] = useState<string | null>(null);
+//     const [items, setItems] = useState<Item[]>([]);
+
+//     // Fetch current week start and end date
+
+//     // Fetch all meals in the week
+
+//     // Fetch all recipes from meals
+
+//     // Compared ingredients from recipe and ingredients in inventory
+
+//     // Display remaining amount need to buy
+
+//     return (
+//         <Button title="Autogenerate Grocery List" onPress={() => console.log("Press me")}/>
+//     );
+// }
+
+// export default AutogenerateGrocery;
